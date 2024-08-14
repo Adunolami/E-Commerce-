d6d05260bb9ab7efe1ae9d73d219803041cec7cb
Adunolami <abinatitilayo2@gmail.com>
2024-08-15 00:13:49 +0100

    Creating-React-app

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href='./'>amazon
+        </a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
